b6a5774c3597b5485e11651268337b1dc312b296
Nakayama Yuhei <nakayama-yuhei@kayac.com>
2015-11-24 19:48:20 +0900

    add redis cookbook

---
+++ cookbooks/redis/recipes/default.rb
@@ -0,0 +1,16 @@
+#
+# Cookbook Name:: redis
+# Recipe:: default
+#
+# Copyright 2015, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "redis" do
+  options "--enablerepo=epel"
+end
+
+service "redis" do
+  action [:enable, :start]
+end
